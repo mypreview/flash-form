f6b0dfa814e9ba9809a2b4d2510ba582fc547b51
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-20 22:36:06 +0300

    `Display` adjustment controls

---
+++ src/components/PanelDisplaySettings.js
@@ -0,0 +1,33 @@
+/**
+ * WordPress dependencies
+ */
+import { PanelBody, ToggleControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Component that renders setting controls specific to `Display` adjustments.
+ *
+ * @param 	  {Object}  	   props               Component properties.
+ * @param 	  {Object}  	   props.attributes    Available block attributes and their corresponding values.
+ * @param 	  {Function}  	   props.onChange	   A callback function invoked when any of the values change.
+ * @return    {JSX.Element}                        Panel control components to render.
+ */
+function PanelDisplaySettings( { attributes, onChange } ) {
+	const { noLabels } = attributes;
+
+	return (
+		<PanelBody initialOpen={ false } title={ __( 'Display Settings', 'flash-form' ) }>
+			<ToggleControl
+				checked={ Boolean( noLabels ) }
+				help={ __(
+					'Enabling this option will visually hide labels. The labels are still visible to screen readers. This option can be overridden for each input field individually.',
+					'flash-form'
+				) }
+				label={ __( 'Hide labels from view?', 'flash-form' ) }
+				onChange={ () => onChange( { noLabels: ! noLabels } ) }
+			/>
+		</PanelBody>
+	);
+}
+
+export default PanelDisplaySettings;
