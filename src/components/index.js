d40d53d5aaa3d9746c3a0d79c837b0f0efa0d206
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-20 22:35:19 +0300

    Exported component scripts

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
+export { default as GetMethodSettings } from './GetMethodSettings';
 export { default as Icon } from './Icon';
 export { default as Inspector } from './Inspector';
+export { default as PanelDisplaySettings } from './PanelDisplaySettings';
+export { default as PanelFormSettings } from './PanelFormSettings';
+export { default as PostMethodSettings } from './PostMethodSettings';
