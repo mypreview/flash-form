51a0ae0739eede877dc00c6ace63c2e47bd0183d
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-20 20:21:52 +0300

    Defined list of allowed blocks

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ */
+export default Object.freeze( {
+	ALLOWED_BLOCKS: [
+		'core/audio',
+		'core/code',
+		'core/cover',
+		'core/gallery',
+		'core/heading',
+		'core/html',
+		'core/image',
+		'core/list',
+		'core/paragraph',
+		'core/quote',
+		'core/separator',
+		'core/shortcode',
+		'core/spacer',
+		'core/subhead',
+		'core/table',
+		'core/verse',
+		'core/video',
+	],
+} );
