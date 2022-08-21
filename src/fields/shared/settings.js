6a49fdafa41e550ab9c3f3988024bab841894345
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:43:35 +0300

    Shared block settings

---
+++ src/fields/shared/settings.js
@@ -0,0 +1,45 @@
+/**
+ * Internal dependencies
+ */
+import Edit from '../Edit';
+import save from '../save';
+
+/**
+ * A field-block requires a few properties to be specified
+ * before it can be registered successfully. These are defined
+ * through the following configuration object, which includes:
+ */
+export default {
+	apiVersion: 2,
+	category: 'flash-form',
+	edit: Edit,
+	parent: [ 'mypreview/flash-form' ],
+	save,
+	supports: {
+		align: false,
+		anchor: true,
+		color: {
+			background: true,
+			gradients: false,
+			link: false,
+			text: true,
+			__experimentalSkipSerialization: true,
+		},
+		html: false,
+		reusable: false,
+		spacing: {
+			margin: [ 'left', 'right' ],
+			padding: true,
+			units: [ 'px', 'em', 'rem' ],
+		},
+		__experimentalBorder: {
+			color: true,
+			radius: true,
+			style: true,
+			width: true,
+			__experimentalSkipSerialization: true,
+		},
+	},
+	textdomain: 'flash-form',
+	usesContext: [ 'formId', 'noLabel' ],
+};
