439e4a241b229375dc3c93ac8b94b35c97b314a1
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:47:12 +0300

    Register sidebar Inspector control(s)

---
+++ src/fields/components/Inspector.js
@@ -0,0 +1,47 @@
+/**
+ * External dependencies
+ */
+import { WidthPanel } from '@mypreview/unicorn-react-components';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { InspectorControls } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Inspector Controls appear in the post settings sidebar when a block is being edited.
+ *
+ * @see 	  https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/block-controls-toolbar-and-sidebar/
+ * @param 	  {Object} 	       props 				  Block meta-data properties.
+ * @param 	  {Object} 	       props.attributes 	  Available block attributes and their corresponding values.
+ * @param     {Function}       props.setAttributes    Function to update individual attributes based on user interactions.
+ * @return    {JSX.Element} 						  Inspector element to render.
+ */
+function Inspector( { attributes, setAttributes } ) {
+	const { width } = attributes;
+
+	return (
+		<InspectorControls>
+			<WidthPanel
+				help={ __( 'Adjust the width of the field to include multiple fields on a single line.', 'flash-form' ) }
+				onChange={ ( value ) => setAttributes( { width: value } ) }
+				title={ __( 'Width settings', 'flash-form' ) }
+				value={ width }
+			/>
+		</InspectorControls>
+	);
+}
+
+Inspector.propTypes = {
+	attributes: PropTypes.object,
+	setAttributes: PropTypes.func,
+};
+
+Inspector.defaultProps = {
+	attributes: {},
+	setAttributes: () => {},
+};
+
+export default Inspector;
