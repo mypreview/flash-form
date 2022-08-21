aff1003668cd1a5390f2c6b8f4a4b8488805bdb5
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:41:23 +0300

    Defined plugin specific block collection(s)

---
+++ src/utils/collections.js
@@ -0,0 +1,14 @@
+/**
+ * WordPress dependencies
+ */
+import { _x } from '@wordpress/i18n';
+
+/**
+ * Array of block collections.
+ */
+export default [
+	{
+		name: 'mypreview-flash-form',
+		settings: { title: _x( 'Flash Form', 'block collection', 'flash-form' ) },
+	},
+];
