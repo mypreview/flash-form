9c9ff77ff5bdab86de78aa939529ca9c7e81241d
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:44:29 +0300

    Import and register block types

---
+++ src/fields/index.js
@@ -0,0 +1,10 @@
+/**
+ * Internal dependencies
+ */
+import { blocks } from './shared';
+import { registerBlockTypes } from './utils';
+
+/**
+ * Function to register inner-blocks provided by this plugin.
+ */
+registerBlockTypes( blocks );
