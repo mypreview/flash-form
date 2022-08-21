4e7a9d2064a586b4fbd191e6511ee7a8778396a9
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:44:16 +0300

    FPO

---
+++ src/fields/save.js
@@ -0,0 +1,15 @@
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see 	  https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+ * @param 	  {Object} 		   props               Component properties.
+ * @param 	  {Object} 		   props.attributes    Block attributes.
+ * @return    {JSX.Element} 					   Form element to render.
+ */
+function save( { attributes } ) {
+	return null;
+}
+
+export default save;
