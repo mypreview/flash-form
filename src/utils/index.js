44d9e8a560c34ed7adeaddc8f5a2a4de966aa740
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:41:31 +0300

    Exported new scripts

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,4 @@
 export { default as baseClassName } from './baseClassName';
+export { default as collections } from './collections';
 export { default as SLUG } from './slug';
+export { default as registerBlockCollections } from './registerBlockCollections';
