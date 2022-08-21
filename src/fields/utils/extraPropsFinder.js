97db87c9136d3d757b2e25b1b5687e241f6f8759
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-22 00:43:25 +0300

    Find extra props attached to the block settings

---
+++ src/fields/utils/extraPropsFinder.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { defaultTo, find, isEqual } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import blocks from '../blocks';
+
+/**
+ * Iterates over blocks of collection.
+ * Returning the first block predicate returns truthy for.
+ *
+ * @function
+ * @name 	  extraPropsFinder
+ * @param 	  {string}    needle    The searched identifier value.
+ * @return    {Array}			    Returns the matched element, else undefined.
+ */
+export default ( needle ) => {
+	const block = find( blocks, ( { extraProps: { identifier } } ) => isEqual( identifier, needle ) );
+	return { extraProps: defaultTo( block?.extraProps, {} ), block };
+};
