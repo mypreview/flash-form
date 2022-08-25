956a2ebdce6d773a8aa013f4433d94e6e17899c3
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-25 15:09:16 +0300

    Simplified

--- src/fields/hooks/useSyncContextValues.js
+++ src/fields/hooks/useSyncContextValues.js
@@ -13,13 +13,13 @@ import { useEffect } from '@wordpress/element';
  *
  * @function
  * @name 	  useSyncContextValues
- * @param     {Object}      context          Context object that comes with a Provider React component.
- * @param     {Function}    setAttributes    Function to update individual attributes based on user interactions.
+ * @param     {Object}      context            Context object that comes with a Provider React component.
+ * @param     {string}      context.formId     Unique id assigned to the form (parent) block.
+ * @param     {boolean}     context.noLabel    Whether the label should only be visible to screen readers.
+ * @param     {Function}    setAttributes      Function to update individual attributes based on user interactions.
  * @return    {void}
  */
-export default ( context, setAttributes ) => {
-	const { formId, noLabel } = context;
-
+export default ( { formId, noLabel }, setAttributes ) => {
 	useEffect( () => {
 		setAttributes( { formId } );
 	}, [ formId ] );
