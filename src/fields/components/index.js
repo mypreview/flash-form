4493fdec1d5d437ed5d9569cb706e299524f748e
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-25 12:48:19 +0300

    Exported component scripts

--- src/fields/components/index.js
+++ src/fields/components/index.js
@@ -4,6 +4,8 @@ export { default as InputControl } from './InputControl';
 export { default as Inspector } from './Inspector';
 export { default as LabelControl } from './LabelControl';
 export { default as PrintChoices } from './PrintChoices';
+export { default as PrintOptions } from './PrintOptions';
+export { default as PrintRadios } from './PrintRadios';
 export { default as RadioControl } from './RadioControl';
 export { default as SelectControl } from './SelectControl';
 export { default as SortableChoices } from './SortableChoices';
