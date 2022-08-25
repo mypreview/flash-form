cf9bdb2e378fbe6eb223d0fe059c149cfd3df67f
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-25 15:08:55 +0300

    Exported hook script

--- src/fields/hooks/index.js
+++ src/fields/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as usePrepareInputProps } from './usePrepareInputProps';
 export { default as usePrepareOnChange } from './usePrepareOnChange';
+export { default as useSyncBorderStyles } from './useSyncBorderStyles';
 export { default as useSyncContextValues } from './useSyncContextValues';
