5394e6e68c7dde04fbb6a9533150187979772185
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-26 23:36:25 +0300

    Remove falsey values from the array

--- src/variations.js
+++ src/variations.js
@@ -3,7 +3,7 @@
  */
 import { insertAtIndex, stringify } from '@mypreview/unicorn-js-utils';
 import { Icon } from '@mypreview/unicorn-react-components';
-import { get, map, nth } from 'lodash';
+import { compact, get, map, nth } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -21,124 +21,126 @@ import icons from './assets/icons.json';
  */
 export default applyFilters(
 	'mypreview.flashFormVariations',
-	map(
-		[
-			{
-				description: __( 'Add a contact form', 'flash-form' ),
-				isDefault: true,
-				innerBlocks: [
-					[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
-					[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
-					[ 'textarea', { label: __( 'Message', 'flash-form' ) } ],
-					[ 'button', { defaultValue: __( 'Contact us', 'flash-form' ) } ],
-				],
-				name: 'contact',
-				scope: [ 'block' ],
-				title: __( 'Contact', 'flash-form' ),
-			},
-			{
-				description: __( 'Add a RSVP form', 'flash-form' ),
-				innerBlocks: [
-					[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
-					[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
-					[
-						'radio',
-						{
-							defaultValue: stringify( [ __( 'Yes', 'flash-form' ), __( 'No', 'flash-form' ) ] ),
-							isRequired: true,
-							label: __( 'Attending?', 'flash-form' ),
-						},
+	compact(
+		map(
+			[
+				{
+					description: __( 'Add a contact form', 'flash-form' ),
+					isDefault: true,
+					innerBlocks: [
+						[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
+						[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
+						[ 'textarea', { label: __( 'Message', 'flash-form' ) } ],
+						[ 'button', { defaultValue: __( 'Contact us', 'flash-form' ), lock: { remove: true } } ],
 					],
-					[ 'textarea', { label: __( 'Other Details', 'flash-form' ) } ],
-					[ 'button', { defaultValue: __( 'Send RSVP', 'flash-form' ) } ],
-				],
-				name: 'rsvp',
-				scope: [ 'block' ],
-				title: __( 'RSVP', 'flash-form' ),
-			},
-			{
-				description: __( 'Add a registration form', 'flash-form' ),
-				innerBlocks: [
-					[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
-					[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
-					[ 'tel', { isRequired: true, label: __( 'Phone', 'flash-form' ) } ],
-					[
-						'select',
-						{
-							defaultValue: stringify( [
-								__( 'Search Engine', 'flash-form' ),
-								__( 'Social Media', 'flash-form' ),
-								__( 'TV', 'flash-form' ),
-								__( 'Radio', 'flash-form' ),
-								__( 'Friend of Family', 'flash-form' ),
-							] ),
-							isRequired: true,
-							label: __( 'How did you hear about us?', 'flash-form' ),
-						},
+					name: 'contact',
+					scope: [ 'block' ],
+					title: __( 'Contact', 'flash-form' ),
+				},
+				{
+					description: __( 'Add a RSVP form', 'flash-form' ),
+					innerBlocks: [
+						[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
+						[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
+						[
+							'radio',
+							{
+								defaultValue: stringify( [ __( 'Yes', 'flash-form' ), __( 'No', 'flash-form' ) ] ),
+								isRequired: true,
+								label: __( 'Attending?', 'flash-form' ),
+							},
+						],
+						[ 'textarea', { label: __( 'Other Details', 'flash-form' ) } ],
+						[ 'button', { defaultValue: __( 'Send RSVP', 'flash-form' ), lock: { remove: true } } ],
 					],
-					[ 'textarea', { label: __( 'Other Details', 'flash-form' ) } ],
-					[ 'button', { defaultValue: __( 'Register', 'flash-form' ) } ],
-				],
-				name: 'registration',
-				scope: [ 'block' ],
-				title: __( 'Registration', 'flash-form' ),
-			},
-			{
-				description: __( 'Add an appointment booking form', 'flash-form' ),
-				innerBlocks: [
-					[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
-					[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
-					[ 'tel', { isRequired: true, label: __( 'Phone', 'flash-form' ) } ],
-					[ 'date', { isRequired: true, label: __( 'Date', 'flash-form' ) } ],
-					[
-						'radio',
-						{
-							defaultValue: stringify( [ __( 'Morning', 'flash-form' ), __( 'Afternoon', 'flash-form' ) ] ),
-							isRequired: true,
-							label: __( 'Time?', 'flash-form' ),
-						},
+					name: 'rsvp',
+					scope: [ 'block' ],
+					title: __( 'RSVP', 'flash-form' ),
+				},
+				{
+					description: __( 'Add a registration form', 'flash-form' ),
+					innerBlocks: [
+						[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
+						[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
+						[ 'tel', { isRequired: true, label: __( 'Phone', 'flash-form' ) } ],
+						[
+							'select',
+							{
+								defaultValue: stringify( [
+									__( 'Search Engine', 'flash-form' ),
+									__( 'Social Media', 'flash-form' ),
+									__( 'TV', 'flash-form' ),
+									__( 'Radio', 'flash-form' ),
+									__( 'Friend of Family', 'flash-form' ),
+								] ),
+								isRequired: true,
+								label: __( 'How did you hear about us?', 'flash-form' ),
+							},
+						],
+						[ 'textarea', { label: __( 'Other Details', 'flash-form' ) } ],
+						[ 'button', { defaultValue: __( 'Register', 'flash-form' ), lock: { remove: true } } ],
 					],
-					[ 'textarea', { label: __( 'Notes', 'flash-form' ) } ],
-					[ 'button', { defaultValue: __( 'Book Appointment', 'flash-form' ) } ],
-				],
-				name: 'appointment',
-				scope: [ 'block' ],
-				title: __( 'Appointment', 'flash-form' ),
-			},
-			{
-				description: __( 'Add a feedback form', 'flash-form' ),
-				innerBlocks: [
-					[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
-					[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
-					[
-						'radio',
-						{
-							defaultValue: stringify( [
-								__( '1- Very Bad', 'flash-form' ),
-								__( '2- Poor', 'flash-form' ),
-								__( '3- Average', 'flash-form' ),
-								__( '4- Good', 'flash-form' ),
-								__( '5- Excellent', 'flash-form' ),
-							] ),
-							isRequired: true,
-							label: __( 'Please rate our website?', 'flash-form' ),
-						},
+					name: 'registration',
+					scope: [ 'block' ],
+					title: __( 'Registration', 'flash-form' ),
+				},
+				{
+					description: __( 'Add an appointment booking form', 'flash-form' ),
+					innerBlocks: [
+						[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
+						[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
+						[ 'tel', { isRequired: true, label: __( 'Phone', 'flash-form' ) } ],
+						[ 'date', { isRequired: true, label: __( 'Date', 'flash-form' ) } ],
+						[
+							'radio',
+							{
+								defaultValue: stringify( [ __( 'Morning', 'flash-form' ), __( 'Afternoon', 'flash-form' ) ] ),
+								isRequired: true,
+								label: __( 'Time?', 'flash-form' ),
+							},
+						],
+						[ 'textarea', { label: __( 'Notes', 'flash-form' ) } ],
+						[ 'button', { defaultValue: __( 'Book Appointment', 'flash-form' ), lock: { remove: true } } ],
 					],
-					[ 'textarea', { label: __( 'How could we improve?', 'flash-form' ) } ],
-					[ 'button', { defaultValue: __( 'Send Feedback', 'flash-form' ) } ],
-				],
-				name: 'feedback',
-				scope: [ 'block' ],
-				title: __( 'Feedback', 'flash-form' ),
-			},
-		],
-		( variation ) => {
-			variation.icon = <Icon d={ get( icons, variation.name ) } />;
-			variation.innerBlocks = map( variation?.innerBlocks, ( innerBlock ) =>
-				insertAtIndex( innerBlock, `mypreview/flash-form-field-${ nth( innerBlock ) }`, 0 )
-			);
-			variation.name = `${ variation.name }-variation`;
-			return variation;
-		}
+					name: 'appointment',
+					scope: [ 'block' ],
+					title: __( 'Appointment', 'flash-form' ),
+				},
+				{
+					description: __( 'Add a feedback form', 'flash-form' ),
+					innerBlocks: [
+						[ 'name', { isRequired: true, label: __( 'Name', 'flash-form' ) } ],
+						[ 'email', { isRequired: true, label: __( 'Email', 'flash-form' ) } ],
+						[
+							'radio',
+							{
+								defaultValue: stringify( [
+									__( '1- Very Bad', 'flash-form' ),
+									__( '2- Poor', 'flash-form' ),
+									__( '3- Average', 'flash-form' ),
+									__( '4- Good', 'flash-form' ),
+									__( '5- Excellent', 'flash-form' ),
+								] ),
+								isRequired: true,
+								label: __( 'Please rate our website?', 'flash-form' ),
+							},
+						],
+						[ 'textarea', { label: __( 'How could we improve?', 'flash-form' ) } ],
+						[ 'button', { defaultValue: __( 'Send Feedback', 'flash-form' ), lock: { remove: true } } ],
+					],
+					name: 'feedback',
+					scope: [ 'block' ],
+					title: __( 'Feedback', 'flash-form' ),
+				},
+			],
+			( variation ) => {
+				variation.icon = <Icon d={ get( icons, variation.name ) } />;
+				variation.innerBlocks = map( variation?.innerBlocks, ( innerBlock ) =>
+					insertAtIndex( innerBlock, `mypreview/flash-form-field-${ nth( innerBlock ) }`, 0 )
+				);
+				variation.name = `${ variation.name }-variation`;
+				return variation;
+			}
+		)
 	)
 );
