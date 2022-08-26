6c21d869ccffc9e4e0c60229b228c536d6146971
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-08-26 17:12:09 +0300

    Exported `NameControl` component script

--- src/fields/components/index.js
+++ src/fields/components/index.js
@@ -3,6 +3,7 @@ export { default as CheckboxesControl } from './CheckboxesControl';
 export { default as InputControl } from './InputControl';
 export { default as Inspector } from './Inspector';
 export { default as LabelControl } from './LabelControl';
+export { default as NameControl } from './NameControl';
 export { default as PrintChoices } from './PrintChoices';
 export { default as PrintOptions } from './PrintOptions';
 export { default as PrintRadios } from './PrintRadios';
