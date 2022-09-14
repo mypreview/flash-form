59d766ae5f4b359bacab9321a39c5cfb4dbec4c6
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-14 20:44:35 +0300

    Exported scripts

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
 export { default as baseClassName } from './baseClassName';
 export { default as collections } from './collections';
+export { default as getAttributes } from './getAttributes';
+export { default as hasGetUrl } from './hasGetUrl';
 export { default as SLUG } from './slug';
 export { default as registerBlockCollections } from './registerBlockCollections';
