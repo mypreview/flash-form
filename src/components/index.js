3d13a264fa55d791068f3d8a9454d937d14c4d79
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-15 23:49:30 +0300

    Exported component script

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { default as Inspector } from './Inspector';
 export { default as PanelDisplaySettings } from './PanelDisplaySettings';
 export { default as PanelFormSettings } from './PanelFormSettings';
 export { default as PanelHoneypotSettings } from './PanelHoneypotSettings';
+export { default as PanelReCaptchaSettings } from './PanelReCaptchaSettings';
 export { default as PostMethodSettings } from './PostMethodSettings';
