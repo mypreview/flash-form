6f62b02d53ff092d97d2b454791d893ca0c44eb3
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-15 23:48:19 +0300

    New helper methods

--- includes/class-utils.php
+++ includes/class-utils.php
@@ -4,7 +4,7 @@
  *
  * @link          https://mypreview.github.io/flash-form
  * @author        MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
- * @since         1.0.0
+ * @since         1.1.0
  *
  * @package       flash-form
  * @subpackage    flash-form/includes
@@ -44,6 +44,18 @@ if ( ! class_exists( 'Utils' ) ) :
 		}
 
 		/**
+		 * Retrieves the current site ISO language code.
+		 *
+		 * @since     1.1.0
+		 * @return    string
+		 */
+		public static function get_language_code(): string {
+			$get_locale = explode( '_', get_locale() );
+
+			return $get_locale[0] ?? 'en';
+		}
+
+		/**
 		 * Filters value of a field or content and strips out disallowed HTML.
 		 *
 		 * @since     1.0.0
@@ -53,6 +65,42 @@ if ( ! class_exists( 'Utils' ) ) :
 		public static function clean( string $value ): string {
 			$value = str_replace( array( '[', ']' ), array( '&#91;', '&#93;' ), $value );
 			return nl2br( \wp_kses( $value, array() ) );
+		}
+
+		/**
+		 * Recursive sanitation for an array.
+		 * Returns the sanitized values of an array.
+		 *
+		 * @since    1.1.0
+		 * @param    array $input    Array of values.
+		 * @return   array
+		 */
+		public static function clean_array( array $input ): array {
+			// Bail early, in case the input value is missing or not an array.
+			if ( empty( $input ) || ! is_array( $input ) ) {
+				return array();
+			}
+
+			// Loop through the array to sanitize each key/values recursively.
+			foreach ( $input as $key => &$value ) {
+				if ( is_array( $value ) ) {
+					$value = self::clean_array( $value );
+				} else {
+					$value = sanitize_text_field( $value );
+				}
+			}
+
+			return $input;
+		}
+
+		/**
+		 * Returns whether the current user has the specified capability.
+		 *
+		 * @since     1.0.0
+		 * @return    bool
+		 */
+		public static function rest_editor_permission_callback(): bool {
+			return \current_user_can( 'edit_posts' );
 		}
 
 	}
