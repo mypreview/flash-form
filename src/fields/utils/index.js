a8fbfcca983839d48c5f9d31553fe5a38e714559
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-20 00:53:05 +0300

    Export `transforms` script

--- src/fields/utils/index.js
+++ src/fields/utils/index.js
@@ -1,2 +1,3 @@
 export { default as extraPropsFinder } from './extraPropsFinder';
 export { default as registerBlockTypes } from './registerBlockTypes';
+export { default as transforms } from './transforms';
