3bc72544227d1044ef6659392fdd13fb9330df45
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-20 20:01:16 +0300

    Exported `dynamicProps` script

--- src/fields/utils/index.js
+++ src/fields/utils/index.js
@@ -1,3 +1,4 @@
+export { default as dynamicProps } from './dynamicProps';
 export { default as extraPropsFinder } from './extraPropsFinder';
 export { default as registerBlockTypes } from './registerBlockTypes';
 export { default as transforms } from './transforms';
