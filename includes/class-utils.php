9aadd53e69827f97967512500078b2ca5dddc452
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-24 13:53:14 +0300

    Added new helper methods

--- includes/class-utils.php
+++ includes/class-utils.php
@@ -56,9 +56,100 @@ if ( ! class_exists( 'Utils' ) ) :
 		}
 
 		/**
+		 * Retrieves post id of given post-object or currently queried object id.
+		 *
+		 * @since     1.1.0
+		 * @param     int|WP_Post|null $post    Post ID or post object.
+		 * @return    int
+		 */
+		public static function get_post_id( $post = null ): ?int {
+			$post_id  = null;
+			$get_post = \get_post( $post, 'OBJECT' );
+
+			if ( is_null( $get_post ) ) {
+				$post_id = (int) \get_queried_object_id();
+			} elseif ( property_exists( $get_post, 'ID' ) ) {
+				$post_id = (int) $get_post->ID;
+			}
+
+			return $post_id;
+		}
+
+		/**
+		 * Post id of the translation if exists, null otherwise.
+		 *
+		 * @since     1.1.0
+		 * @param     null|int $post_id    Post ID.
+		 * @return    null|int
+		 */
+		public static function get_localized_post_id( ?int $post_id = null ): ?string {
+			$return = null;
+
+			if ( self::is_post_exists( $post_id ) ) {
+				$return = $post_id;
+				if ( self::is_polylang_activated() ) {
+					$pll_post_id = \pll_get_post( $post_id );
+					if ( $pll_post_id && ! is_null( $pll_post_id ) ) {
+						$return = (int) $pll_post_id;
+					}
+				}
+			}
+
+			return $return;
+		}
+
+		/**
+		 * Query a third-party plugin activation.
+		 * This statement prevents from producing fatal errors,
+		 * in case the the plugin is not activated on the site.
+		 *
+		 * @since     1.1.0
+		 * @param     string $slug        Plugin slug to check for the activation state.
+		 * @param     string $filename    Optional. Plugin’s main file name.
+		 * @return    bool
+		 * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+		 */
+		public static function is_plugin_activated( string $slug, string $filename = '' ): bool {
+			$filename               = empty( $filename ) ? $slug : $filename;
+			$plugin_path            = \apply_filters( 'mypreview_flash_form_third_party_plugin_path', sprintf( '%s/%s.php', \esc_html( $slug ), \esc_html( $filename ) ) );
+			$subsite_active_plugins = \apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+			$network_active_plugins = \apply_filters( 'active_plugins', get_site_option( 'active_sitewide_plugins' ) );
+
+			// Bail early in case the plugin is not activated on the website.
+			// phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict
+			if ( ( empty( $subsite_active_plugins ) || ! in_array( $plugin_path, $subsite_active_plugins ) ) && ( empty( $network_active_plugins ) || ! array_key_exists( $plugin_path, $network_active_plugins ) ) ) {
+				return false;
+			}
+
+			return true;
+		}
+
+		/**
+		 * Query the "Polylang" plugin activation.
+		 *
+		 * @since     1.1.0
+		 * @return    bool
+		 */
+		public static function is_polylang_activated(): bool {
+			return self::is_plugin_activated( 'polylang' ) || self::is_plugin_activated( 'polylang-pro', 'polylang' );
+		}
+
+		/**
+		 * Determines if a post, identified by the specified ID,
+		 * exist within the WordPress database.
+		 *
+		 * @since     1.1.0
+		 * @param     null|int $post_id    Post ID.
+		 * @return    bool
+		 */
+		public static function is_post_exists( ?int $post_id = null ): bool {
+			return ! is_null( $post_id ) && is_string( \get_post_type( $post_id ) );
+		}
+
+		/**
 		 * Filters value of a field or content and strips out disallowed HTML.
 		 *
-		 * @since     1.0.0
+		 * @since     1.1.0
 		 * @param     string $value    Content or value of the field.
 		 * @return    string
 		 */
