2a73dd10900134360e1d445c95bcf063c21a3393
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-25 18:03:24 +0300

    Determines whether the entries post type is registered

--- dashboard/class-entries.php
+++ dashboard/class-entries.php
@@ -124,6 +124,11 @@ if ( ! class_exists( Entries::class ) ) :
 		 * @return    void
 		 */
 		public function add_admin_page(): void {
+			// Bail early, in case the post-type is unregistered.
+			if ( ! self::is_post_type_exists() ) {
+				return;
+			}
+
 			\add_menu_page(
 				// Page title.
 				__( 'Flash Form', 'flash-form' ),
@@ -174,6 +179,11 @@ if ( ! class_exists( Entries::class ) ) :
 		 * @return    void
 		 */
 		public function save( array $args ): void {
+			// Bail early, in case the post-type is unregistered.
+			if ( ! self::is_post_type_exists() ) {
+				return;
+			}
+
 			$author          = (string) $args['headers']['raw']['author'] ?? '';
 			$author_email    = (string) $args['headers']['raw']['author_email'] ?? '';
 			$subject         = (string) $args['headers']['raw']['subject'] ?? '';
@@ -393,7 +403,7 @@ if ( ! class_exists( Entries::class ) ) :
 		 * @return    string
 		 * @phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
 		 */
-		public static function svg_icon_url(): string {
+		private static function svg_icon_url(): string {
 			return 'data:image/svg+xml;base64,' . base64_encode( '<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="m.11 11.905c0-6.567 5.324-11.891 11.89-11.891.04 0 .096-.002.165-.005.51-.018 1.725-.063 1.639.512l-1.101 7.337a.1.1 0 00.088.114l7.387.821a.353.353 0 01.261.539l-1.816 2.905a.353.353 0 01-.35.163l-11.727-1.675a.353.353 0 01-.24-.551l6-8.67c-5.782-.166-10.71 4.61-10.71 10.4 0 1.181.197 2.316.56 3.374.187.55-.117.962-.421 1.375l-.096.13a.292.292 0 01-.506-.046 11.85 11.85 0 01-1.023-4.832zm8.347-2.25a.1.1 0 01-.069-.156l3.006-4.342c.12-.172.39-.067.363.14l-.368 2.764c-.12.802.053 1.142.858 1.232l6.084.633a.1.1 0 01.074.152l-.538.861a.1.1 0 01-.099.046l-9.311-1.33zm15.435 2.448c0 6.679-5.48 11.736-11.995 11.893-.441.01-1.767.042-1.683-.515l1.1-7.337a.1.1 0 00-.087-.114l-7.388-.821a.353.353 0 01-.26-.539l1.816-2.906a.353.353 0 01.35-.162l11.727 1.675c.26.037.39.335.24.551l-6.006 8.675c5.78.16 10.7-4.615 10.7-10.4 0-1.18-.197-2.315-.56-3.373-.188-.55.117-.962.421-1.376l.096-.13a.292.292 0 01.506.047 11.849 11.849 0 011.023 4.832zm-8.33 2.244a.1.1 0 01.067.155l-3.006 4.342c-.12.173-.39.068-.362-.14l.368-2.764c.12-.801-.053-1.142-.859-1.231l-6.083-.633a.1.1 0 01-.074-.152l.538-.862a.1.1 0 01.099-.046l9.311 1.33zm1.208-9.527a.267.267 0 00-.465-.144l-1.805 2.028a.267.267 0 00.175.444l2.088.19c.17.015.311-.13.29-.3l-.283-2.218zm-9.54 14.37a.267.267 0 00.465.144l1.805-2.026a.267.267 0 00-.176-.444l-2.088-.19a.267.267 0 00-.29.3l.285 2.218z"/></svg>' );
 		}
 
@@ -412,6 +422,16 @@ if ( ! class_exists( Entries::class ) ) :
 				unset( $unread[ $needle ] );
 				\update_option( self::$unread_option, $unread );
 			}
+		}
+
+		/**
+		 * Determines whether the entries post type is registered.
+		 *
+		 * @since     1.1.0
+		 * @return    bool
+		 */
+		private static function is_post_type_exists() {
+			return \post_type_exists( self::$post_type );
 		}
 
 	}
