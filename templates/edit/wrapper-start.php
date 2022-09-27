b9bf73f2c51869ba25574c52d3325c213696f869
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-27 21:59:50 +0300

    Table wrapper start tag(s)

---
+++ templates/edit/wrapper-start.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template part for displaying the start of the block table element.
+ *
+ * @link          https://mypreview.github.io/flash-form
+ * @author        MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ * @since         1.1.0
+ *
+ * @package       flash-form
+ * @subpackage    flash-form/templates/edit
+ */
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+?>
+<table class="widefat fixed striped">
+
+<?php
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
